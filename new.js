bdd942b26ec605802a70678f0b5c9f556807269e
AsHiK-M(C.D) <ashik018361@gmaill.com>
2023-03-23 10:20:59 +0600

    tane number code  updeted page

---
+++ new.js
@@ -0,0 +1,14 @@
+// const animals = ['cat', 'dog', 'rat'];
+// console.log(animals.includes('Cat'));
+// const unique = array => array.indexOf(21);
+// const array = unique[2,3,4,5,2];
+// console.log(array)
+// unique();
+// const add =(para1 , para2) => para2 + para1;
+//  const result = add('2', 3);
+//  console.log(result)
+// const b=6
+// b=6
+// console.log(b);
+const colors = {mango: 'green', grapes: 'black', organe: 'yellow'};
+console.log(colors.grapes)
